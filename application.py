4ab480a5fe72f399f28bb6d6d819aee9b5c97867
Mirko Rüther <mirkoruether@users.noreply.github.com>
2021-06-08 13:19:11 +0200

    Deployment stuff

---
+++ application.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Mirko Ruether
+"""
+
+from mcrecipegraph.web.app import flask_app as application # pylint: disable=unused-import
+
+if __name__ == "__main__":
+    application.run()
---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Mirko Ruether
+"""
+
+from mcrecipegraph.web.app import flask_app as app # pylint: disable=unused-import
+
+if __name__ == "__main__":
+    app.run()
